07dfd2d3b47c6fccafc758204b07cb2c9ab87842
Shreya Bhatia <shreya@Shreya-Bhatias-MacBook-Pro.local>
2011-03-22 18:56:57 +0530

    Initial commit

---
+++ csv1.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'csv'
+
+new_objects = []
+@@meths = [] 
+csv = CSV.open("./#{ARGV[0]}.csv", 'r', ',')
+
+class_name = ARGV[0].capitalize
+klass = Object.const_set(class_name,Class.new)
+
+csv.shift.map {|method|
+    @@meths << method
+    klass.class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+      attr_accessor :#{method} 
+    EOS
+}
+
+klass.class_eval do
+  def initialize(*args)  
+    args.each_with_index do |arg, index|
+      instance_variable_set :"@#{@@meths[index]}", arg
+    end        
+  end       
+end
+
+
+new_objects = csv.inject([]) do |posts, row| 
+    new_objects << klass.new(*row)
+end
+csv.close
+
+p new_objects
---
+++ csv3.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'csv'
+
+class_name = ARGV[0].capitalize
+klass = Object.const_set(class_name,Class.new)
+
+columns = []
+csv = CSV.open("./#{ARGV[0]}.csv", 'r', ',')
+csv.shift.map {|method|
+  columns << method
+}
+
+
+klass.class_eval do
+  attr_accessor *columns
+  define_method(:initialize) do |*values|
+    columns.each_with_index do |name, index|
+      instance_variable_set("@"+name.to_s, values[index])
+    end
+  end
+end
+
+new_objects = []
+new_objects = csv.inject([]) do |posts, row| 
+    new_objects << klass.new(*row)
+end
+csv.close
+
+p new_objects
